89c159ab703b22120bc29226aba82d1faa6a615d
plscallMeAlex <alexkung007@gmail.com>
2024-09-25 01:38:43 +0700

    custom httpexception

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPExceptions
 from middleware import add_middleware
 from routers.routes import router as api_router
 
@@ -8,6 +9,11 @@ app.include_router(api_router)
 
 # add middleware
 add_middleware(app)
+
+# custom exception handler
+@app.exception_handler(Exception)
+async def custom_exception_handler(request:Request, exc:StarletteHTTPExceptions):
+    return JSONResponse(content={"success":False,"error_msg":str(exc)}, status_code=exc.status_code)
 
 # Program health check
 @app.get("/")
