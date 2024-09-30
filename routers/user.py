dd6e043204cb3d6bf39488c2beb3be4849ed3403
plscallMeAlex <alexkung007@gmail.com>
2024-10-01 00:17:17 +0700

    fixing user detail schema

--- routers/user.py
+++ routers/user.py
@@ -18,22 +18,3 @@ async def login(user:UserLogin, db:Session=Depends(db_dependency)):
 @router.post("/register")
 async def register(user_create: UserCreate, db: Session = Depends(db_dependency)):
     return user_crud.user_register(user_create, db)
-                                                                       
-@router.get("/get_user", response_model=UserResponse)
-async def read_user(
-    id: str = None,
-    username: str = None,
-    firstname: str = None,
-    db: Session = Depends(db_dependency)
-):
-    # Use `db` to interact with the database
-    if id is not None:
-        db_user = db.query(User).filter(User.id == id).first()
-    elif username is not None:
-        db_user = db.query(User).filter(User.username == username).first()
-    elif firstname is not None:
-        db_user = db.query(User).filter(User.firstname == firstname).first()
-    else:
-        raise HTTPException(status_code=404, detail="User not found")
-    
-    return db_user
