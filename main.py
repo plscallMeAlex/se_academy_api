d6285aaa87e6decc35d53d804fd175bb6eb2fac8
Bthcorn <bowornthat.maize@gmail.com>
2024-10-06 18:09:51 +0700

    refactor: gitignore and main

--- main.py
+++ main.py
@@ -10,58 +10,7 @@ app = FastAPI()
 app.include_router(api_router)
 
 # add middleware
-origins = ["http://localhost", "http://127.0.0.1", "http://127.0.0.1:5173"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*", "Authorization"],
-    expose_headers=["Authorization"],
-)
-
-app.mount(
-    "/images",
-    StaticFiles(directory="images"),
-    name="images",
-)
-
-
-# @app.middleware("http")
-# async def check_token(request: Request, call_next):
-#     # Will delete the path docs and openapi later
-#     if (
-#         (request.url.path == "/" and request.method == "GET")
-#         or (request.url.path == "/user/login")
-#         or (request.url.path == "/user/register")
-#         or (request.url.path == "/docs" and request.method == "GET")
-#         or (request.url.path == "/openapi.json" and request.method == "GET")
-#     ):
-#         response = await call_next(request)
-#         return response
-
-#     token = request.headers.get("Authorization")
-
-#     if token is None:
-#         return JSONResponse(
-#             content={"success": False, "error_msg": "Token is missing"},
-#             status_code=401,
-#         )
-
-#     try:
-#         check_token_valid(token)
-#     except HTTPException as e:
-#         return JSONResponse(
-#             content={"success": False, "error_mg": str(e.detail)},
-#             status_code=e.status_code,
-#         )
-
-#     response = await call_next(request)
-#     return response
-
-
-# add_middleware(app)
+add_middleware(app)
 
 app.mount(
     "/images",
