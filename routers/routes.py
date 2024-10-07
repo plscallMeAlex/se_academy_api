aa0c518263f7fb891e15696456b5ec41f445cd9e
plscallMeAlex <alexkung007@gmail.com>
2024-10-07 22:47:07 +0700

    add enrolled to route

--- routers/routes.py
+++ routers/routes.py
@@ -2,7 +2,9 @@
 from fastapi import APIRouter
 from .user import router as users
 from .course import router as course
+from .enrolled import router as enrolled
 
 router = APIRouter()
 router.include_router(users, prefix="/user", tags=["user"])
 router.include_router(course, prefix="/course", tags=["course"])
+router.include_router(enrolled, prefix="/enrolled_course", tags=["enrolled"])
